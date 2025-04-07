42e8b895f9b6a1778b6709e0783bdd9d69aa6037
Brayan Camilo <130872257+Lebraiann@users.noreply.github.com>
2025-04-07 20:19:22 +0000

    kcyo

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css'
+
+import Aleatorios from './Componentes/aleatorios'
+import  Capturados from './Componentes/capturados'  
+import Favoritos from './Componentes/favoritos'
+import Lista from './Componentes/lista' 
+import Pokemon from './Componentes/pokemon'
+import Usuario from './Componentes/usuarios'
+import Menu from './Componentes/menu';
+
+function App() {
+
+  return (
+    <Router>
+
+<Menu />
+
+
+<Routes>
+  <Route path="/" element={<Lista />} />
+  <Route path="/usuarios" element={<Usuario />} />
+  <Route path="/aleatorios" element={<Aleatorios />} />
+  <Route path="/capturados" element={<Capturados />} />
+  <Route path="/favoritos" element={<Favoritos />} />
+</Routes>
+
+</Router>
+ 
+  )
+}
+
+export default App
